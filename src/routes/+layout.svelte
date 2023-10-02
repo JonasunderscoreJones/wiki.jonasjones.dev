45e2657cd1638cbadb5b335f18f6dd959c8abcf4
Jonas_Jones <91549607+J-onasJones@users.noreply.github.com>
2023-10-02 18:56:34 +0200

    Removed garbage code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,23 +2,16 @@
     import Header from "$lib/components/Header.svelte";
     import Footer from "$lib/components/Footer.svelte";
     import Navbar from "$lib/components/Navbar.svelte";
+
 </script>
 
 <div style="height:2rem" />
 <Header />
 <div class="container">
     <div class="column"><Navbar /></div>
-    <div class="column content flex_grow" style="margin-left: 0;">
+    <div class="column content flex_grow">
         <main>
             <slot />
-            <!-- Please god forgive me -->
-            <h1
-                style="color: var(--container-background-color);margin:0;padding:0"
-                class="unselectable"
-            >
-                YOU CANT SEE THIS YOU CANT SEE THIS YOU CANT SEE THIS YOU CANT
-                SEE THIS
-            </h1>
         </main>
     </div>
 </div>

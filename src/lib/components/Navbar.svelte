fc76f264401356a0bb90bb746908f6aa0ba706d8
Jonas_Jones <91549607+J-onasJones@users.noreply.github.com>
2023-10-14 21:21:33 +0200

    Added box shadow for search field

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -7,7 +7,7 @@
 
 <div class="container navbar">
 	<div class="row">
-		<input type="text" bind:value={searchterm} placeholder="Search Pages (Alpha)..." />
+		<input class="box-shadow-inverted" type="text" bind:value={searchterm} placeholder="Search Pages (Alpha)..." />
 		<div class="col-md-12">
 			<h2>Pages</h2>
 			<div class="navbar-links">
